5443507795867655c6fbe5275cdf1f129fecfb43
Abinavbhagat <anuradha.na25@gmail.com>
2024-06-23 17:46:44 +0530

    service class modified

--- src/main/java/com/core/service/EmployeeService.java
+++ src/main/java/com/core/service/EmployeeService.java
@@ -1,7 +1,12 @@
 package com.core.service;
 
+import com.core.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private Employee employee;
 }
